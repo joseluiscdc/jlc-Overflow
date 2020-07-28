a3f1864272d9a6543944b3872ccc2cda1a60544c
José Luis <joseluis.jlc@gmail.com>
2020-07-28 11:36:38 -0400

    Config deploy to production

--- server/app.js
+++ server/app.js
@@ -18,9 +18,12 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 if (process.env.NODE_ENV === 'production') {
-	res.setHeader('Access-Control-Allow-Origin', '*')
-	res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept')
-	res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS')
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept')
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS')
+    next()
+  })
 	app.use(express.static(path.join(process.cwd(), 'dist/jlc-overflow')))
 }
 
