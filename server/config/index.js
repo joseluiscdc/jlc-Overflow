7f0164928802fa97a3c650010872650f9093bdff
José Luis <joseluis.jlc@gmail.com>
2020-07-28 10:11:20 -0400

    Config deploy to production

--- server/config/index.js
+++ server/config/index.js
@@ -4,7 +4,7 @@ const config = {
   secret: process.env.SECRET_JWT,
   dev: process.env.NODE_ENV !== 'prod',
   port: process.env.PORT || 3000,
-  mongoUrl: process.env.URL_MONGO.toString(),
+  mongoUrl: process.env.MONGODB_URI.toString(),
 };
 
 module.exports = { config }
