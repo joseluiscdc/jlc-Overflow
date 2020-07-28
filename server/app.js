5775526d5ad30de82b3fc56953160140cc1abbea
José Luis <joseluis.jlc@gmail.com>
2020-07-28 13:18:08 -0400

    Config deploy to production

--- server/app.js
+++ server/app.js
@@ -25,6 +25,9 @@ if (process.env.NODE_ENV === 'production') {
     next()
   })
 	app.use(express.static(path.join(process.cwd(), 'dist/jlc-overflow')))
+  app.get('/*', function(req, res) {
+    res.sendFile('index.html', { root: 'dist/jlc-overflow/' });
+  })
 }
 
 app.use('/api/questions', question)
