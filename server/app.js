944aabf4041335ca32db9ef7c18fa5dd40639a72
José Luis <joseluis.jlc@gmail.com>
2020-07-28 09:56:42 -0400

    Config deploy to production

--- server/app.js
+++ server/app.js
@@ -3,17 +3,22 @@ const bodyParser = require("body-parser");
 const question = require('./routes/question')
 const auth = require('./routes/auth')
 const app = express()
+const path = require('path')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'development') {
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept')
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS')
     next()
   })
+}
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(process.cwd(), 'dist')))
 }
 
 app.use('/api/questions', question)
